a07556ebd85c90190fb97ca79bfd7e85caa73e23
robertkraaijeveld <0890289@hr.nl>
2017-03-29 16:12:33 +0200

    put back install steps

--- buildScripts/installation.sh
+++ buildScripts/installation.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv 
 echo 'export PATH=\"$HOME/.rbenv/bin:$PATH\"' >> ~/.bashrc 
-  
+     
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build 
 
 RUBY_CONFIGURE_OPTS=--enable-shared rbenv install 2.2.2 
