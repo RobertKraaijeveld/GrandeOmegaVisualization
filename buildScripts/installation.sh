9418a701a850b571a2816ea5129ddb4c0b3383fd
robertkraaijeveld <0890289@hr.nl>
2017-03-29 16:56:00 +0200

    jenkins github plugin test

--- buildScripts/installation.sh
+++ buildScripts/installation.sh
@@ -4,9 +4,5 @@ echo 'export PATH=\"$HOME/.rbenv/bin:$PATH\"' >> ~/.bashrc
 export PATH=$PATH:/usr/local/bin:$HOME/.rbenv/bin:$HOME/.rbenv/shims
 eval "$(rbenv init -)"
 
-rbenv local
-rbenv rehash
-RUBY_CONFIGURE_OPTS=--enable-shared rbenv install 2.2.2 
-
-sudo gem install rails
-sudo gem install rice
+gem install rails
+gem install rice
