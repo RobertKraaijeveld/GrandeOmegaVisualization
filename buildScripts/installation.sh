f9b3d199d3c77d70d2a2096bf8d407e3692e4d38
robertkraaijeveld <0890289@hr.nl>
2017-03-29 17:51:26 +0200

    removed gem install

--- buildScripts/installation.sh
+++ buildScripts/installation.sh
@@ -4,5 +4,5 @@ echo 'export PATH=\"$HOME/.rbenv/bin:$PATH\"' >> ~/.bashrc
 export PATH=$PATH:/usr/local/bin:$HOME/.rbenv/bin:$HOME/.rbenv/shims
 eval "$(rbenv init -)"
 
-gem install rails
-gem install rice
+#gem install rails
+#gem install rice
