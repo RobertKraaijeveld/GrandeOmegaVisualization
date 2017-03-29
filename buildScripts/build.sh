91353c914e4a3f9079b116f3fd5ee16a634f275f
robertkraaijeveld <0890289@hr.nl>
2017-03-29 12:45:06 +0200

    Added C++11 to travis and split up build steps

---
+++ buildScripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+systemArchitecture=$(uname -m) 
+
+cd ../ext/
+
+if [ "$systemArchitecture" = "x86_64" ]; then
+ echo "64 bit system, using 64 bit makefile..."
+ make -f makefile64bit 
+else
+ echo "32 bit system, using 32 bit makefile.."
+ make -f makefile32bit 
+fi
---
+++ buildScripts/installations.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH=\"$HOME/.rbenv/bin:$PATH\"' >> ~/.bashrc
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+RUBY_CONFIGURE_OPTS=--enable-shared rbenv install 2.2.2
+
+gem install rail,
+gem install rice
