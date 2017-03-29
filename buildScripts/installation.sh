559fefdcf154961956e784241990ce556f21888e
robertkraaijeveld <0890289@hr.nl>
2017-03-29 16:20:14 +0200

    moar jenkins testing yay

--- buildScripts/installation.sh
+++ buildScripts/installation.sh
@@ -1,7 +1,7 @@
-#git clone https://github.com/rbenv/rbenv.git ~/.rbenv 
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv 
 echo 'export PATH=\"$HOME/.rbenv/bin:$PATH\"' >> ~/.bashrc 
        
-#git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build 
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build 
 
 RUBY_CONFIGURE_OPTS=--enable-shared rbenv install 2.2.2 
 
