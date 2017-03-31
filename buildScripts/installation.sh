391e1875e165c6bf36e3c5003815d7b642a27707
robertkraaijeveld <0890289@hr.nl>
2017-03-31 14:00:39 +0200

    Backup

--- buildScripts/installation.sh
+++ buildScripts/installation.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo 'export PATH=\"$HOME/.rbenv/bin:$PATH\"' >> ~/.bashrc        
-export PATH=$PATH:/usr/local/bin:$HOME/.rbenv/bin:$HOME/.rbenv/shims
-eval "$(rbenv init -)"
+#install rbenv and ruby 2.2.2 with enable shared
+#echo 'export PATH=\"$HOME/.rbenv/bin:$PATH\"' >> ~/.bashrc        
+#export PATH=$PATH:/usr/local/bin:$HOME/.rbenv/bin:$HOME/.rbenv/shims
+
+#install libpqxx, lib-cppunit
 
 #gem install rails
 #gem install rice
