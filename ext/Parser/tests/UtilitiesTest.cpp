281a94c74c87f3295e4300b6e966b100506c7338
robert <0890289@hr.nl>
2017-04-17 15:16:12 +0200

    Added Cppunit support

---
+++ ext/Parser/tests/UtilitiesTest.cpp
@@ -0,0 +1,15 @@
+#include <cppunit/TestCase.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/TestCaller.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+#include "UtilitiesTest.h"
+
+using namespace CppUnit;
+using namespace std;
+
+void UtilitiesTest::testIsNCharsWhiteSpace()
+{
+    //Utilities::isNCharsWhiteSpace(int N, string& str)
+}
